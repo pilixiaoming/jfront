aaed909781852edaaea32f84a7b61c3e61250864
pilixiaoming <pilixiaoming@gmail.com>
2015-12-26 20:05:09 +0800

    initial commit

---
+++ dasheng_v1/jijiyan/admin3/user-dashboard-app.js
@@ -0,0 +1,24 @@
+﻿
+
+var userDashboardApp = angular.module('userDashboardApp', ['ngRoute']);
+
+//This configures the routes and associates each route with a view and a controller
+userDashboardApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/view1',
+            {
+                controller: 'AddressController',
+                templateUrl: 'view1.html'
+            })
+        //Define a route that has a route parameter in it (:customerID)
+        .when('/view2',
+            {
+              //  controller: 'CustomersController',
+                templateUrl: 'view2.html'
+            })
+        .otherwise({ redirectTo: '/view1' });
+});
+
+
+
+
---
+++ dasheng_v1/js/linechart.js
@@ -0,0 +1,75 @@
+
+function drawLineChart(dataProvider, divId)
+{
+    var chart = AmCharts.makeChart(divId, {
+        "type": "serial",
+        "theme": "light",
+        "marginRight": 80,
+        "autoMarginOffset": 20,
+        "dataDateFormat": "YYYY-MM-DD",
+
+
+        "valueAxes": [{
+            "id": "v1",
+            "axisAlpha": 0,
+            "position": "left"
+        }],
+        "balloon": {
+            "borderThickness": 1,
+            "shadowAlpha": 0
+        },
+        "graphs": [{
+            "id": "g1",
+            "bullet": "round",
+            "bulletBorderAlpha": 1,
+            "bulletColor": "#FFFFFF",
+            "bulletSize": 5,
+            "hideBulletsCount": 50,
+            "lineThickness": 2,
+            "title": "red line",
+            "useLineColorForBulletBorder": true,
+            "valueField": "value",
+            "balloonText": "<div style='margin:5px; font-size:19px;'><span style='font-size:13px;'>[[category]]</span><br>[[value]]</div>"
+        }],
+        "chartScrollbar": {
+            "graph": "g1",
+            "oppositeAxis": false,
+            "offset": 30,
+            "scrollbarHeight": 80,
+            "backgroundAlpha": 0,
+            "selectedBackgroundAlpha": 0.1,
+            "selectedBackgroundColor": "#888888",
+            "graphFillAlpha": 0,
+            "graphLineAlpha": 0.5,
+            "selectedGraphFillAlpha": 0,
+            "selectedGraphLineAlpha": 1,
+            "autoGridCount": true,
+            "color": "#AAAAAA"
+        },
+        "chartCursor": {
+            "pan": true,
+            "valueLineEnabled": true,
+            "valueLineBalloonEnabled": true,
+            "cursorAlpha": 0,
+            "valueLineAlpha": 0.2
+        },
+        "categoryField": "date",
+        "categoryAxis": {
+            "parseDates": true,
+            "dashLength": 1,
+            "minorGridEnabled": true
+        },
+        "export": {
+            "enabled": true
+        },
+        "dataProvider": dataProvider
+    });
+
+    chart.addListener("rendered", zoomChart);
+
+    zoomChart();
+
+    function zoomChart() {
+        chart.zoomToIndexes(chart.dataProvider.length - 40, chart.dataProvider.length - 1);
+    }
+}
